c4e73ba1b25428e0e9b81ceac4dbcd077a66ae1a
diogojme <diogojme@gmail.com>
2016-09-30 10:12:19 -0300

    Initial commit

---
+++ app/src/main/java/inf/ufg/br/mydoctor/MainActivity.java
@@ -0,0 +1,14 @@
+package inf.ufg.br.mydoctor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
